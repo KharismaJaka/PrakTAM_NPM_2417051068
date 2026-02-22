f6de0eb1b715dbd2de8d58f5663717e469da6c1e
Kharisma Jaka Harum <kjakaharum@gmail.com>
2026-02-22 13:48:33 +0700

    Initial commit: Setup Modul 2

---
+++ app/src/main/java/com/example/praktam_npm_2417051068/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.praktam_npm_2417051068
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.praktam_npm_2417051068.ui.theme.PrakTAM_NPM_2417051068Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PrakTAM_NPM_2417051068Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello saya $name dengan npm $npm siap belajar compose",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PrakTAM_NPM_2417051068Theme {
+        Greeting("Android")
+    }
+}
